2a706e9922494a7671d3be91fc41cf88a5428769
Divyansh <divi09112@gmail.com>
2019-03-24 21:49:53 +0530

    correct column name in table results

--- initdb.sh
+++ initdb.sh
@@ -60,7 +60,7 @@ create_tables() {
     CREATE TABLE IF NOT EXISTS rooms(roomid VARCHAR PRIMARY KEY, qm VARCHAR, state VARCHAR, FOREIGN KEY(qm) REFERENCES qms(username));
     CREATE TABLE IF NOT EXISTS users(username VARCHAR PRIMARY KEY, email VARCHAR, phone VARCHAR, roomid VARCHAR, socket VARCHAR, FOREIGN KEY(roomid) REFERENCES rooms(roomid));
     CREATE TABLE IF NOT EXISTS questions(id SERIAL PRIMARY KEY, roomid VARCHAR, question TEXT, options JSON, answer INTEGER, FOREIGN KEY(roomid) REFERENCES rooms(roomid));
-    CREATE TABLE IF NOT EXISTS results(id SERIAL PRIMARY KEY, userid VARCHAR, roomid VARCHAR, attempts JSON, total INTEGER, FOREIGN KEY(userid) REFERENCES users(username), FOREIGN KEY(roomid) REFERENCES rooms(roomid));
+    CREATE TABLE IF NOT EXISTS results(id SERIAL PRIMARY KEY, username VARCHAR, roomid VARCHAR, attempts JSON, total INTEGER, FOREIGN KEY(userid) REFERENCES users(username), FOREIGN KEY(roomid) REFERENCES rooms(roomid));
 EOSQL
 }
 
