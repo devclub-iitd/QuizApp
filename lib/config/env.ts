2a270b2187d8e52ef91610b1f62157120efeb1a5
Divyansh <divi09112@gmail.com>
2019-04-03 22:37:53 +0530

    Create default admin(creds from environment) on first setup

--- lib/config/env.ts
+++ lib/config/env.ts
@@ -1,4 +1,8 @@
 export = {
     PORT: process.env.PORT || 3001,
     DATABASE_URL: process.env.DATABASE_URL || 'postgres://testuser:testpass@localhost:5432/test',
+    QM_USER: process.env.QM_USER || 'admin',
+    QM_PASSWORD: process.env.QM_PASSWORD || 'password',
+    QM_EMAIL: process.env.QM_EMAIL || 'abc@abc.com',
+    QM_PHONE: process.env.QM_PHONE || '1234567',
 }
--- lib/config/tables.ts
+++ lib/config/tables.ts
@@ -1,9 +1,12 @@
+import env = require('./env');
+
 const initQueries: string[] = [
     'CREATE TABLE IF NOT EXISTS qms(username VARCHAR PRIMARY KEY, email VARCHAR, phone VARCHAR, password VARCHAR, socket VARCHAR);',
     'CREATE TABLE IF NOT EXISTS rooms(roomid VARCHAR PRIMARY KEY, qm VARCHAR, state VARCHAR, FOREIGN KEY(qm) REFERENCES qms(username));',
     'CREATE TABLE IF NOT EXISTS users(username VARCHAR PRIMARY KEY, email VARCHAR, phone VARCHAR, roomid VARCHAR, socket VARCHAR, FOREIGN KEY(roomid) REFERENCES rooms(roomid));',
     'CREATE TABLE IF NOT EXISTS questions(id SERIAL PRIMARY KEY, roomid VARCHAR, question TEXT, options JSON, answer INTEGER, FOREIGN KEY(roomid) REFERENCES rooms(roomid));',
     'CREATE TABLE IF NOT EXISTS results(id SERIAL PRIMARY KEY, username VARCHAR, roomid VARCHAR, attempts JSON, total INTEGER, FOREIGN KEY(username) REFERENCES users(username), FOREIGN KEY(roomid) REFERENCES rooms(roomid));',
+    'INSERT INTO QMS(username, email, phone, password) values(\''+env.QM_USER+'\',\''+env.QM_EMAIL+'\',\''+env.QM_PHONE+'\',\''+env.QM_PASSWORD+'\') ON CONFLICT DO NOTHING;',
 ]
 
 export = initQueries;
